079c981f560e19ec4133b1ecfb1e67deedc7d044
nemesisratus <nemesisratus@gmail.com>
2024-11-20 08:20:41 +0700

    Update and rename training.py to commit.py

--- commit.py
+++ commit.py
@@ -3,9 +3,9 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/text')
+os.chdir('/content/48pm')
 
-full_local_path = "/content/text"
+full_local_path = "/content/48pm"
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
